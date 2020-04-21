4ce7529fa3c92f9066e86b654747af736b09b08c
Jon Kuperman <jon.kuperman@gmail.com>
2020-04-21 13:18:52 -0400

    fix sign up form

--- src/components/mailchimp.js
+++ src/components/mailchimp.js
@@ -4,11 +4,21 @@ import addToMailchimp from 'gatsby-plugin-mailchimp';
 const EmailListForm = () => {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setError('');
+        setSuccess('');
         addToMailchimp(email, {
-            FNAME: name
+            FNAME: name,
+        }).then((result) => {
+            if (result.result === 'error') {
+                setError(result.msg);
+            } else if (result.result === 'success') {
+                setSuccess(result.msg);
+            }
         });
     };
 
@@ -21,28 +31,26 @@ const EmailListForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h2>Subscribe to my email list!</h2>
-            <div className="mailchimpForm" style={{ margin: `50px 0` }}>
-                <label>Name:
-                <input
-                        name="name"
-                        id="name"
-                        type="text"
-                        onChange={handleNameChange}
-                    />
-                </label>
-                <label>Email:
-                <input
-                        name="email"
-                        id="email"
-                        type="text"
-                        onChange={handleEmailChange}
-                    />
-                </label>
-                <button className="mailchimpSubmit" type="submit">Subscribe</button>
-            </div>
-        </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <h2>Subscribe to my email list!</h2>
+                {error && <div>{error}</div>}
+                {success && <div>{success}</div>}
+                <div className="mailchimpForm" style={{ margin: `50px 0` }}>
+                    <label>
+                        Name:
+                        <input name="name" id="name" type="text" onChange={handleNameChange} />
+                    </label>
+                    <label>
+                        Email:
+                        <input name="email" id="email" type="text" onChange={handleEmailChange} />
+                    </label>
+                    <button className="mailchimpSubmit" type="submit">
+                        Subscribe
+                    </button>
+                </div>
+            </form>
+        </>
     );
 };
 
