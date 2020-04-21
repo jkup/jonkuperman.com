9b4ceacbb7e679275c520bf50ad6f5212c1d5b87
Jon Kuperman <jon.kuperman@gmail.com>
2020-04-21 14:08:08 -0400

    add favicon

--- src/components/tagCloud.js
+++ src/components/tagCloud.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import kebabCase from 'lodash/kebabCase';
+import { Link } from 'gatsby';
 
 const TagCloud = ({ tags }) => {
     return (
         <section>
             <h2>Categories</h2>
+            {tags.map((tag) => (
+                <li key={tag.tag}>
+                    <Link to={`/tags/${kebabCase(tag.tag)}/`}>
+                        {tag.tag} ({tag.totalCount})
+                    </Link>
+                </li>
+            ))}
         </section>
     );
 };
