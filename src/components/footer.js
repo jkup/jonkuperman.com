ce09b43669db50f6574efdce18bbee21c9445845
Jon Kuperman <jon.kuperman@gmail.com>
2020-05-03 18:14:56 -0400

    add back to top button

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,36 @@
-import React from "react"
+import React from 'react';
 import Mailchimp from './mailchimp';
+import { css } from '@emotion/core';
 
 const Footer = () => {
+    const handleOnClick = function () {
+        window.scrollTo(0, 0);
+    };
+
     return (
         <footer>
             <Mailchimp />
-            <span>© {new Date().getFullYear()}, Jon Kuperman</span>
+            <span style={{ textAlign: 'left' }}>© {new Date().getFullYear()}, Jon Kuperman</span>
+            <button
+                css={css`
+                    textalign: right;
+                    background: transparent;
+                    border: none;
+                    color: #fff;
+                    cursor: pointer;
+                    float: right;
+
+                    &:hover {
+                        color: #08f7fe;
+                    }
+                `}
+                href="#"
+                onClick={handleOnClick}
+            >
+                Back to top &uarr;
+            </button>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
