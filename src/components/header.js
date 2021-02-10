bf48d7ef421e8b447d80f856f7e9879f807de107
Jon Kuperman <jon.kuperman@gmail.com>
2021-02-09 21:07:00 -0500

    added new darkmode toggle

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,46 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import DarkModeToggle from 'react-dark-mode-toggle';
 
 const Header = (props) => {
-    const switcher = useRef(null);
-    const [checked, setChecked] = useState(false);
+    const [isDarkMode, setDarkMode] = useState(() => false);
 
-    const handleChange = (e) => {
-        if (e.target.checked) {
+    const toggleDarkMode = (isDarkMode) => {
+        if (isDarkMode) {
             document.documentElement.setAttribute('data-theme', 'dark');
             localStorage.setItem('theme', 'dark');
+            setDarkMode(true);
         } else {
             document.documentElement.setAttribute('data-theme', 'light');
             localStorage.setItem('theme', 'light');
+            setDarkMode(false);
         }
-        setChecked(!checked);
     };
 
     useEffect(() => {
         const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+        const prefersLightScheme = window.matchMedia('(prefers-color-scheme: light)');
 
         if (currentTheme) {
             document.documentElement.setAttribute('data-theme', currentTheme);
 
             if (currentTheme === 'dark') {
-                setChecked(true);
+                setDarkMode(true);
             }
+        } else if (prefersLightScheme.matches) {
+            document.documentElement.setAttribute('data-theme', 'light');
+            setDarkMode(false);
         }
-    }, [checked]);
+    }, [isDarkMode]);
 
     return (
         <>
             <div style={{ display: 'flow-root' }}>
                 <div style={{ float: 'right' }} className="theme-switch-wrapper">
-                    <label className="theme-switch" htmlFor="checkbox">
-                        <input
-                            ref={switcher}
-                            onChange={handleChange}
-                            type="checkbox"
-                            id="checkbox"
-                            checked={checked}
-                        />
-                        <div className="slider round"></div>
-                    </label>
+                    <DarkModeToggle
+                        onChange={(isDarkMode) => toggleDarkMode(isDarkMode)}
+                        checked={isDarkMode}
+                        size={80}
+                    />
                 </div>
             </div>
             {props.children}
