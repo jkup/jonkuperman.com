a57587c82be617775d659d5cd82b0b2b0fb0ba9d
Jon Kuperman <jon.kuperman@gmail.com>
2021-10-27 13:01:31 -0400

    eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -75,6 +75,7 @@ module.exports = function (eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img")
   eleventyConfig.addPassthroughCopy("css")
+  eleventyConfig.addPassthroughCopy({ static: "/" })
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
