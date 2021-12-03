19a0e148a68be101e48e4a66ac103173dfda4f2e
Jon Kuperman <jon.kuperman@gmail.com>
2021-12-03 15:33:51 -0500

    typo

--- js/likes.js
+++ js/likes.js
@@ -13,17 +13,17 @@ fetch("/likes", {
     count.innerText = data
   })
 
-  button.addEventListener("click", (event) => {
-    event.preventDefault()
-    fetch("/likes", {
-      method: "POST",
-      body: JSON.stringify({
-        path: pathname.split("/")[1],
-        operation: "increment"
-      }),
+button.addEventListener("click", event => {
+  event.preventDefault()
+  fetch("/likes", {
+    method: "POST",
+    body: JSON.stringify({
+      path: pathname.split("/")[1],
+      operation: "increment",
+    }),
+  })
+    .then(response => response.text())
+    .then(data => {
+      count.innerText = data
     })
-      .then(response => response.text())
-      .then(data => {
-        count.innerText = data
-      })
-  }
+})
