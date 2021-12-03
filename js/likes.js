7aea610169db9c569693c124139d8bc5ffbef1f2
Jon Kuperman <jon.kuperman@gmail.com>
2021-12-03 15:43:02 -0500

    checks

--- js/likes.js
+++ js/likes.js
@@ -10,20 +10,24 @@ fetch("/likes", {
 })
   .then(response => response.text())
   .then(data => {
-    count.innerText = data
+    if (count) {
+      count.innerText = data
+    }
   })
 
-button.addEventListener("click", event => {
-  event.preventDefault()
-  fetch("/likes", {
-    method: "POST",
-    body: JSON.stringify({
-      path: pathname.split("/")[1],
-      operation: "increment",
-    }),
-  })
-    .then(response => response.text())
-    .then(data => {
-      count.innerText = data
+if (button) {
+  button.addEventListener("click", event => {
+    event.preventDefault()
+    fetch("/likes", {
+      method: "POST",
+      body: JSON.stringify({
+        path: pathname.split("/")[1],
+        operation: "increment",
+      }),
     })
-})
+      .then(response => response.text())
+      .then(data => {
+        count.innerText = data
+      })
+  })
+}
