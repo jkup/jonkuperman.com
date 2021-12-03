1cd69738e5c5c9fd01c693cb255e3508c0826dd2
Jon Kuperman <jon.kuperman@gmail.com>
2021-12-03 15:22:47 -0500

    some updates

--- functions/likes.ts
+++ functions/likes.ts
@@ -1,24 +1,29 @@
 export const onRequest: PagesFunction<{
   COUNTER: DurableObjectNamespace
 }> = async ({ request, env }) => {
-  // Generate unique DO for each path
-  let data: any = await request.json()
-  let path = data.body.path
-  let operation = data.body.operation || "/"
+  try {
+    // Generate unique DO for each path
+    let data: any = await request.json()
+    let path = data.body.path
+    let operation = data.body.operation || "/"
 
-  let id = env.COUNTER.idFromName(path)
-  let obj = env.COUNTER.get(id)
+    let id = env.COUNTER.idFromName(path)
+    let obj = env.COUNTER.get(id)
 
-  let resp = await obj.fetch(request.url, {
-    method: "POST",
-    body: JSON.stringify({
-      url: request.url,
-      path: path,
-      operation: operation,
-    }),
-  })
+    let resp = await obj.fetch(request.url, {
+      method: "POST",
+      body: JSON.stringify({
+        url: request.url,
+        path: path,
+        operation: operation,
+      }),
+    })
 
-  let count = await resp.text()
+    let count = await resp.text()
 
-  return new Response(count)
+    return new Response(count)
+  } catch (e) {
+    console.error(e)
+    return new Response(e.message, { status: 500 })
+  }
 }
