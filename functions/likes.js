28e60ef8c74a3402a4e5448da4294922d113b967
Jon Kuperman <jon.kuperman@gmail.com>
2021-12-03 14:09:30 -0500

    add likes

---
+++ functions/likes.js
@@ -0,0 +1,23 @@
+export const onRequest = () => {
+  // Generate unique DO for each path
+  let path = new URL(request.url).pathname.split("/")[1]
+  let operation = new URL(request.url).pathname.split("/")[2] || "/"
+
+  let id = env.COUNTER.idFromName(path)
+  let obj = env.COUNTER.get(id)
+
+  let resp = await obj.fetch(request.url, {
+    method: "POST",
+    body: JSON.stringify({
+      url: request.url,
+      path: path,
+      operation: operation,
+    }),
+  })
+
+  let count = await resp.text()
+
+  return new Response(
+    `${path} recieved operation ${operation} and is now at ${count}`
+  )
+}
