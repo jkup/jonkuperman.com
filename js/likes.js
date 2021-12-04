ce47bdadd6ad540706bfded360cc5ec57a377739
Jon Kuperman <jon.kuperman@gmail.com>
2021-12-03 20:22:15 -0500

    cosmetic updates

--- js/likes.js
+++ js/likes.js
@@ -1,34 +1,39 @@
 const button = document.getElementById("likeButton")
+const likeBanner = document.getElementById("likeBanner")
 const count = document.getElementById("likeCount")
+const likeBannerCount = document.getElementById("likeBannerCount")
 const pathname = window.location.pathname
 const durable_object = `https://jon-kuperman-do.jkup.workers.dev`
 
-fetch(durable_object, {
-  method: "POST",
-  body: JSON.stringify({
-    path: pathname.split("/")[1],
-  }),
-})
-  .then(response => response.text())
-  .then(data => {
-    if (count) {
-      count.innerText = `This post has ${data} likes`
-    }
+function fetchAndUpdate(operation) {
+  fetch(durable_object, {
+    method: "POST",
+    body: JSON.stringify({
+      path: pathname.split("/")[1],
+      ...(operation && { operation }),
+    }),
   })
+    .then(response => response.text())
+    .then(data => {
+      if (count) {
+        count.innerText = `This post has ${data} likes`
+
+        likeBannerCount.innerText = `(${data} likes)`
+      }
+    })
+}
 
 if (button) {
-  button.addEventListener("click", event => {
-    event.preventDefault()
-    fetch(durable_object, {
-      method: "POST",
-      body: JSON.stringify({
-        path: pathname.split("/")[1],
-        operation: "increment",
-      }),
-    })
-      .then(response => response.text())
-      .then(data => {
-        count.innerText = `This post has ${data} likes`
-      })
-  })
+  button.addEventListener("click", handleClick)
 }
+
+if (likeBanner) {
+  likeBanner.addEventListener("click", handleClick)
+}
+
+function handleClick(event) {
+  event.preventDefault()
+  fetchAndUpdate("increment")
+}
+
+fetchAndUpdate()
