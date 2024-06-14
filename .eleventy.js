ba12554b08c26e38576667fcdf32a9d1bbc38101
Jon Kuperman <jon.kuperman@gmail.com>
2024-06-15 00:55:39 +0300

    Add cool particlejs thing and change gradients

--- .eleventy.js
+++ .eleventy.js
@@ -73,7 +73,7 @@ module.exports = function (eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img")
   eleventyConfig.addPassthroughCopy("css")
-  eleventyConfig.addPassthroughCopy("js")
+  eleventyConfig.addPassthroughCopy("scripts")
   eleventyConfig.addPassthroughCopy({ static: "/" })
 
   // Customize Markdown library and settings:
