621545c386530c571670ed401c33e60aad12ba5b
Jon Kuperman <jon.kuperman@gmail.com>
2025-10-03 19:24:02 +0100

    progressing

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,13 @@ const isDark = document.documentElement.dataset.theme === "dark" ||
 button.setAttribute("aria-pressed", isDark ? false : true);
 document.documentElement.dataset.theme = isDark ? "dark" : "light";
 
-const handleSync = () => {
-  if (!document.startViewTransition) return sync();
-  document.startViewTransition(sync);
-};
-
 const sync = () => {
   const darkNow = button.matches("[aria-pressed=false]");
-  console.log(`was ${document.documentElement.dataset.theme}`);
   document.documentElement.dataset.theme = darkNow ? "light" : "dark";
-  console.log(`now ${document.documentElement.dataset.theme}`);
-  console.log(`Now with ${darkNow ? "Dark" : "Light"} Mode.`);
   button.setAttribute("aria-pressed", darkNow ? true : false);
 };
 
-button.addEventListener("click", handleSync);
+button.addEventListener("click", sync);
 
 // Hamburger menu functionality
 const menuToggle = document.querySelector(".menu-toggle");
